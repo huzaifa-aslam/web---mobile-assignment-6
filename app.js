080fe3ae9ee73171b00adf9db4bfd4f5067e3a1a
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-06-21 22:07:38 +0500

    files added

---
+++ app.js
@@ -0,0 +1,72 @@
+// Assignment # 35-38
+
+// 1-
+
+// function dateAndTime(){
+//     var date=new Date()
+//     document.write(date)
+// }
+// dateAndTime()
+
+// 2-
+
+// function greeting(fname,lname){
+//     document.write(`Welcome ${fname} ${lname}`)
+// }
+// greeting('Huzaifa','Aslam')
+
+// 3-
+
+// function sum(){
+//     var c=+prompt('Enter first number')
+//     var d=+prompt("Enter second number")
+//     return c+d
+// }
+// document.write(sum())
+
+// 4-
+
+// function calculator(c,d,f) {
+//   var c = +prompt("Enter first number");
+//   var d = +prompt("Enter second number");
+//   var f = prompt("Enter operator");
+//   switch (f) {
+//     case "+":
+//       return c + d;
+//       break;
+//     case "-":
+//       return c - d;
+//       break;
+//     case "*":
+//       return c * d;
+//       break;
+//     case "/":
+//       return c / d;
+//       break;
+//       default:
+//           return "Try Again"
+
+//   }
+//   return c + d;
+// }
+// document.write(calculator(2,4,"-"));
+
+// 5-
+
+// function square(a,b){
+// return Math.pow(a,b)
+// }
+// document.write(square(2,3))
+
+// 6-
+// function factorial(){
+//     var inputNumber = prompt('Please enter an integer');
+//     var total = 1;
+
+//     for (i = 0; i < inputNumber; i++){
+//         total = total * (inputNumber - i);
+//     }
+
+//     console.log(inputNumber + '! = ' + total);
+// }
+// factorial()
