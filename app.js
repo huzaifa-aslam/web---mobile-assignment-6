ba07ffab25eb8b33716af0473cf25ae1beb9dbbf
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-06-23 23:35:33 +0500

    files updated

--- app.js
+++ app.js
@@ -70,3 +70,60 @@
 //     console.log(inputNumber + '! = ' + total);
 // }
 // factorial()
+
+// 7-
+
+// function counting(){
+//     var a=prompt('enter start number')
+//     var b=prompt(`enter end number`)
+//     for(var i=a;i<=b;i++){
+//         document.write(`${i} <br>`)
+//     }
+// }
+// counting()
+
+// 8-
+
+// function outerFunc(){
+//     var a=+prompt('enter the value of base ')
+//     var b=+prompt(`enter the value of perb`)
+//     var hyp=a*a+b*b;
+
+//  function result(){
+//     document.write(Math.sqrt(hyp))
+//  }
+//  result()
+// }
+// outerFunc()
+
+// 9-
+// function area (widht,height){
+//     var cal=widht*height
+//     document.write(cal)
+// }
+// area(2,4)
+
+// 10-
+
+// function palindrom(string) {
+//   var str = string;
+//   var palindrom="";
+
+//     for(var i=str.length-1;i>=0;i--){
+//         palindrom+=str[i]
+
+//     }
+//     if(str===palindrom){
+//         console.log(`${str} is Palindrome word`)
+//     }
+
+// }
+// palindrom("civic");
+
+// 11-
+function changeCase(str){
+    // var a=str.charAt(0)
+    console.log(a[0].toUpperCase()+)
+    console.log(a)
+}
+changeCase('huzaifa');
